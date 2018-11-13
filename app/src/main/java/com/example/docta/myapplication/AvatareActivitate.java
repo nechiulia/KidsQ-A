9f90a1cbbb5b65d54b88a64584027fe7db576456
nechitaiulia <nechiulia@gmail.com>
2018-11-13 17:43:39 +0200

    +4

--- app/src/main/java/com/example/docta/myapplication/AvatareActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/AvatareActivitate.java
@@ -3,7 +3,7 @@ package com.example.docta.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Avatare extends AppCompatActivity {
+public class AvatareActivitate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/docta/myapplication/IntrebariActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/IntrebariActivitate.java
@@ -3,11 +3,11 @@ package com.example.docta.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class IntrebareaZilei extends AppCompatActivity {
+public class IntrebariActivitate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitate_intrebarea_zilei);
+        setContentView(R.layout.activitate_intrebari);
     }
 }
