56c1668f8c9ea48b96e82dbd6a861dda95dea2a6
Alex Neagu <hzl.alex@yahoo.com>
2018-11-13 17:20:18 +0200

    --

---
+++ app/src/main/java/com/example/docta/myapplication/ContElevActivitate.java
@@ -0,0 +1,53 @@
+package com.example.docta.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ContElevActivitate extends AppCompatActivity {
+
+    private Button btn_back;
+    private Button btn_autentificare;
+    private Button btn_crearecont;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitate_cont_elev);
+
+
+         btn_back=findViewById(R.id.loginelev_btn_back);
+
+         btn_back.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 intent=new Intent(getApplicationContext(),PrincipalaActivitate.class);
+                 startActivity(intent);
+             }
+         });
+
+         btn_autentificare=findViewById(R.id.loginelev_btn_autentificare);
+
+         btn_autentificare.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 intent=new Intent(getApplicationContext(),AutentificareElevActivitate.class);
+                 startActivity(intent);
+             }
+         });
+
+         btn_crearecont=findViewById(R.id.loginelev_btn_crearecont);
+
+         btn_crearecont.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 intent=new Intent(getApplicationContext(),CreareContElevActivitate.class);
+                 startActivity(intent);
+             }
+         });
+
+    }
+}
---
+++ app/src/main/java/com/example/docta/myapplication/CreareContElevActivitate.java
@@ -0,0 +1,37 @@
+package com.example.docta.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class CreareContElevActivitate extends AppCompatActivity {
+
+    private Button btn_back;
+    private Spinner spn_varsta;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitate_creare_cont_elev);
+
+        btn_back=findViewById(R.id.lecc_btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(getApplicationContext(),ContElevActivitate.class);
+                startActivity(intent);
+            }
+        });
+
+        spn_varsta=findViewById(R.id.lecc_spinner_varsta);
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(getApplicationContext(),R.array.lecc_spn_varsta,R.layout.support_simple_spinner_dropdown_item);
+        spn_varsta.setAdapter(adapter);
+
+
+    }
+}
