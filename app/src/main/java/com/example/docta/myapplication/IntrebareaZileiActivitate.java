19f58676bf43fefbbf63958b7ee76e12be4bf58a
AndradaNedelcu <andranedelcu23@gmail.com>
2018-11-16 01:56:01 +0200

    15.11 andra

--- app/src/main/java/com/example/docta/myapplication/IntrebareaZileiActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/IntrebareaZileiActivitate.java
@@ -1,14 +1,22 @@
 package com.example.docta.myapplication;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class IntrebareaZileiActivitate extends AppCompatActivity {
 
     private Button btn_rezultat;
+    private TextView countdown_text;
+    private Button btn_countdown;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMiliseconds=600000; //10 min
+    private boolean timerunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,59 @@ public class IntrebareaZileiActivitate extends AppCompatActivity {
     private void initComponents(){
 
         btn_rezultat=findViewById(R.id.intrebarea_zilei_btn_confirm);
+        countdown_text=findViewById(R.id.intrebarea_zilei_tv_countdown);
+        btn_countdown=findViewById(R.id.intrebarea_zilei_btn_incepe_test);
+
+        btn_countdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startStop();
+            }
+        });
     }
+
+    private void startStop(){
+        if(timerunning){
+            //stopTimer();
+        }else{
+            startTimer();
+        }
+    }
+
+    private void startTimer(){
+        countDownTimer=new CountDownTimer(timeLeftInMiliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                btn_countdown.setVisibility(View.INVISIBLE);
+                timeLeftInMiliseconds=millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        timerunning=true;
+    }
+
+
+
+    private void updateTimer(){
+        int minutes=(int) timeLeftInMiliseconds/60000;
+        int seconds=(int) timeLeftInMiliseconds%60000/1000;
+
+        String timeLeftText;
+
+        timeLeftText=""+minutes;
+        timeLeftText+=":";
+        if(seconds<10) timeLeftText+="0";
+        timeLeftText+=seconds;
+
+        countdown_text.setText(timeLeftText);
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/docta/myapplication/SetariDesignActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/SetariDesignActivitate.java
@@ -4,17 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class SetariDesignActivitate extends AppCompatActivity {
 
     private Button btn_back;
+    private Spinner spn_fonturi;
+    private Spinner spn_dimensiuni;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitate_setari_design);
+
+        spn_fonturi=findViewById(R.id.design_spn_font);
+        ArrayAdapter<CharSequence> adapter_font=ArrayAdapter.createFromResource(getApplicationContext(), R.array.setari_design_spn_font_text, R.layout.support_simple_spinner_dropdown_item);
+        spn_fonturi.setAdapter(adapter_font);
+
+        spn_dimensiuni=findViewById(R.id.design_spn_textsize);
+        ArrayAdapter<CharSequence> adapter_dimensiuni=ArrayAdapter.createFromResource(getApplicationContext(), R.array.setari_design_spn_dimensiuni_text, R.layout.support_simple_spinner_dropdown_item);
+        spn_dimensiuni.setAdapter(adapter_dimensiuni);
 
         btn_back=findViewById(R.id.design_btn_back);
 
