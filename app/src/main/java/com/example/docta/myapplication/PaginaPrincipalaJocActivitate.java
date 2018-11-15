59891934d513789ebb27b549a11be3a3a2b8b6f0
AndradaNedelcu <andranedelcu23@gmail.com>
2018-11-15 18:32:36 +0200

    testare activitati andrada

--- app/src/main/java/com/example/docta/myapplication/PaginaPrincipalaJocActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/PaginaPrincipalaJocActivitate.java
@@ -118,7 +118,7 @@ public class PaginaPrincipalaJocActivitate extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AvatareActivitate.class);
+                Intent intent = new Intent(getApplicationContext(), AvatareleMeleActivitate.class);
                 startActivity(intent);
             }
 
