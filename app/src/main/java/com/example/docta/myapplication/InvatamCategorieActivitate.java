dd6acb8c4296825e0878d502181f5573195cc3ed
Georgiana29 <georgiana.neata29@gmail.com>
2018-11-16 03:27:21 +0200

    Modificari-invatam categorii

--- app/src/main/java/com/example/docta/myapplication/InvatamCategorieActivitate.java
+++ app/src/main/java/com/example/docta/myapplication/InvatamCategorieActivitate.java
@@ -1,94 +1,71 @@
 package com.example.docta.myapplication;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class InvatamCategorieActivitate extends AppCompatActivity {
-    private ImageButton imgBtnBack;
     private ImageButton imgBtnNext;
     private TextView tvNr;
     private ImageView imagineInfo;
     private TextView tvInfo;
+    private Button btnInapoi;
+    private static int nrCurent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitate_invatam_categorie);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         initComponents();
     }
+
    private void initComponents(){
-       imgBtnBack=findViewById(R.id.invatam_categ_imgBtn_inapoi);
        imgBtnNext=findViewById(R.id.invatam_categ_imgBtn_inainte);
        tvNr=findViewById(R.id.invatam_categ_tv_nr_curent);
        tvInfo=findViewById(R.id.invatam_categ_tv_informatie);
        imagineInfo=findViewById(R.id.invatam_categ_imgBtn);
-       if(tvNr.getText().toString().equals("1/10")){
-           imgBtnBack.setVisibility(View.INVISIBLE);
-       }
-       else if (tvNr.getText().toString().equals("10/10")){
-           imgBtnBack.setVisibility(View.VISIBLE);
-           Toast.makeText(getApplicationContext(),getString(R.string.invatam_categorie_toast_felicitari), Toast.LENGTH_LONG);
-       }
-       else {
-           imgBtnBack.setVisibility(View.VISIBLE);
-       }
+       btnInapoi=findViewById(R.id.invatam_categ_btn_inapoi);
+       nrCurent=1;
        imgBtnNext.setOnClickListener(nextEvent());
-       imgBtnBack.setOnClickListener(backEvent());
+       btnInapoi.setOnClickListener(backEvent());
    }
+
+    @TargetApi(21)
    private View.OnClickListener nextEvent() {
        return new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               //String nrCurent = tvNr.getText().toString().substring(0, 1);
-               tvNr.setText(getString(R.string.test_nr_poza));
-               //imagineInfo.setImageResource(R.drawable.imagine_de_informare2);
-               tvInfo.setText(getString(R.string.test_informare_next) );
-               if(tvNr.getText().toString().equals("1/10")){
-                   imgBtnBack.setVisibility(View.INVISIBLE);
-               }
-               else if (tvNr.getText().toString().equals("10/10")){
-                   imgBtnBack.setVisibility(View.VISIBLE);
+               nrCurent++;
+               if (nrCurent == 11) {
+                   Toast.makeText(getApplicationContext(), getString(R.string.invatam_categorie_toast_felicitari), Toast.LENGTH_LONG).show();
                    finish();
-
-               }
-               else {
-                   imgBtnBack.setVisibility(View.VISIBLE);
+               } else {
+                   tvNr.setText(nrCurent + getString(R.string.test_nr_poza));
+                   imagineInfo.setImageDrawable(getDrawable(R.drawable.imagine_de_informare2));
+                   tvInfo.setText(getString(R.string.test_informare_next));
                }
            }
        };
 
    }
+
     private View.OnClickListener backEvent() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tvNr.setText(getString(R.string.test_nr_poza_back));
-                if(tvNr.getText().toString().equals("1/10")){
-                    imgBtnBack.setVisibility(View.INVISIBLE);
-                }
-                else if (tvNr.getText().toString().equals("10/10")){
-                    imgBtnBack.setVisibility(View.VISIBLE);
-                    finish();
-
-                }
-                else {
-                    imgBtnBack.setVisibility(View.VISIBLE);
-                }
-
-                //imagineInfo.setImageDrawable(Drawable.createFromPath("imagine_de_informare2"));
-                tvInfo.setText(getString(R.string.test_informare_back) );
-
-            }
-        };
-
-    }
+            finish();
+        }
+    };}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
