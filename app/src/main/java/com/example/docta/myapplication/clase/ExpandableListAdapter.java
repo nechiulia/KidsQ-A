a3b8ee8dd0faa070be46f3af9d842d03bd922984
DosBrains <nechiulia@gmail.com>
2018-11-17 15:44:47 +0200

    Merge branch 'master' of https://github.com/nechiulia/MyApplication into ionutDinSite
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/docta/myapplication/IntrebareaZileiActivitate.java
    #	app/src/main/java/com/example/docta/myapplication/IntrebariActivitate.java
    #	app/src/main/res/layout/activitate_intrebarea_zilei.xml
    #	app/src/main/res/layout/activitate_pagina_principala_joc.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/docta/myapplication/clase/ExpandableListAdapter.java
+++ app/src/main/java/com/example/docta/myapplication/clase/ExpandableListAdapter.java
@@ -7,9 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
-
 import com.example.docta.myapplication.R;
-
 import java.util.HashMap;
 import java.util.List;
 
